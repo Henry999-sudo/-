49dc8f6f56dd9df6499141cbf0befcf31a4c2f9a
1595199614@qq.com <1595199614@qq.com>
2020-09-22 21:11:14 +0800

    git commit -m "第二次提交"

--- Robocup.cpp
+++ Robocup.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 class message_manager
 {
 private:
-    string messages;
-    string time_hear;
-    string time_see;
+	
+    string messages;                   //消息原始字符串
+    string time
     string sender;
-    string objlnfo;
+    string message;
+    vector<string> total;              //经过处理后的消息分段存入vector中
+	vector<string> objlnfos;
     /* data */
 public:
     message_manager(string message);
@@ -19,8 +22,29 @@ public:
 */
 message_manager::message_manager(string message)
 {
-    messages.append(message, 1, message.size()-2);
-    cout << messages;
+	message.replace(message.size()-1, 1, " ");
+    messages.append(message.replace(0, 1, " "));
+    messages.replace(messages.find("see")-2, 2, " ");
+    
+    int i = 0;
+	while (i < messages.size())                                 //拆分字符串存入vector
+    {
+        ++i;
+        string temp = "";
+        while (messages[i] != ' '&&i < messages.size())
+        {
+            temp.append(messages,i,1);
+            ++i;
+        }
+		total.push_back(temp);
+	}
+	
+	vector<string>::iterator itt = total.begin();             //迭代器访问vector
+	while(itt != total.end())
+	{
+		cout << *itt << "##";  
+		++itt;
+	}
 }
 
 message_manager::~message_manager()
@@ -29,7 +53,7 @@ message_manager::~message_manager()
 
 int main()
 {
-	string s = "(awiudgawiugfiuawhfoiawh)";
+	string s = "(hear udgawi ugf)(see awhfoiawh)";
     message_manager s1(s);
     
     return 0;
