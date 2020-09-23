1a34eda1880339e2dd14c902a77e330af764583c
1595199614@qq.com <1595199614@qq.com>
2020-09-23 19:23:13 +0800

    提交完成

--- Robocup.cpp
+++ Robocup.cpp
@@ -1,6 +1,10 @@
+#ifndef FIRST_HOMEWORK_OF_ROBOCUP
+#define FIRST_HOMEWORK_OF_ROBOCUP
 #include <iostream>
 #include <string>
 #include <vector>
+
+#endif // !FIRST_HOMEWORK_OF_ROBOCUP
 using namespace std;
 class message_manager
 {
@@ -9,68 +13,106 @@ private:
     string messages;                   //消息原始字符串
     string messages_see;
     string messages_hear;
-    string time;
-    string sender;
-    string message;
-    vector<string> HEAR;              //经过处理后的消息分段存入vector中
+	vector<string> HEAR;              //经过处理后的消息分段存入vector中
 	vector<string> SEE;
-    vector<string> objlnfos;          
+	
+    string time;                      //周期                      
+    
+    string sender;                    //听到的信息
+    string message;
+    
+    vector<string> ball;              //看到的信息
+    vector<string> flag;
+    vector<string> Line;
+    vector<string> goal;
+    vector<string> teammates;
+    vector<string> opponent;
+    
     
 public:
     message_manager(string message);
     ~message_manager();
     void message_print();
 };
-/*(hear 1022 -30 passto(23,24))(see 1022 ((ball) -20 20 1 -2) ((player hfut1 2) 45 23 0.5 1 22
-40 ) ((goal r) 12 20) ((Line r) -30))
-*/
+
+/**
+ * description : 将原始消息切割分类存入小数组并且实现格式化输出
+ */
 void message_manager::message_print()
 {
-    cout << "在" << time <<  " 周期 hear 从 " << sender << "方向 听到了 " << message << endl;
+    cout << "在 " << time << "周期" << *(HEAR.begin()) << " 从 " << sender << "方向听到了" << message << ";" << endl;  
+	cout << "在 " << time << "周期" << *(SEE.begin());
+    vector<string>::iterator itt = SEE.begin()+2;
     
-    cout << "在" << time << " 周期 see ";
-    vector<string>::iterator itt = SEE.begin()-2;
     while (itt != SEE.end())
     {
         vector<string> container;
-        container.push_back(*itt.)
-
-        int index = *itt.find(")");
-        while (index < *itt.size())
+		int first = (*itt).find('(')+2;
+        int end = (*itt).find(')');
+        string message_name = (*itt).substr(first, end - first); 
+        
+	 	container.push_back(message_name);                          
+		
+		int index = end+1;
+        while (index < (*itt).size())                              //现将原始消息切割成小段方便但会输出
         {
-            string temp;
-            while (*itt[index] != ' '&&*itt[index] != '('&&itt[index] != ')'&&index < *itt.size())
+            string temp = "";
+            while ((*itt)[index] != ' ' && (*itt)[index] != '(' && (*itt)[index] != ')' && index < (*itt).size())
             {
-                temp.append(*itt[index]);
+                temp.append((*itt).substr(index, 1));
                 index++;
             }
-            container.push_back(temp);
+            if (temp != "")
+            {
+            	container.push_back(temp);
+			}
+			
             index++;
         }
-
-        if (*itt.find("ball") != -1)
+        	
+        if ((*itt).find("ball") != (*itt).npos)                 //将SEE到的信息存储到对应数组并且格式化输出
         {
-            cout << " Ball 距离我的Direction是 " << container[1] << ", Distance是" << container[2] 
-                    << ", Distchng是" << container[3] << ", Dirchng是" << container[4]; 
+        	ball.push_back(*itt);                               //储存原始消息字符串到相应数组
+            
+			cout << " Ball 距离我的Direction是 " << container[1] << ", Distance是" << container[2] 
+                    << ", Distchng是" << container[3] << ", Dirchng是" << container[4] << ";"; 
         }
-        if (*itt.find("player") != -1)
+        else if ((*itt).find("player") != (*itt).npos)
         {
-            cout << container[0] << "距离我的Direction是 " << container[1] << ", Distance是" 
-                    << container[2] << ", Dischng是" << container[3] << "Dirchng是" << container[4]
-                            << "它的BodyDir是 " << container[5] << "，HeadDir是 " << container[6];
+        	if ((*itt).find("hfut1") != (*itt).npos)
+        	{
+        		teammates.push_back(*itt);
+			}
+			else if ((*itt).find("hfut2") != (*itt).npos)
+			{
+				opponent.push_back((*itt));
+			}
+        	
+            cout << container[0] << " 距离我的Direction是 " << container[1] << ", Distance是" 
+                    << container[2] << ", Dischng是" << container[3] << " Dirchng是 " << container[4]
+                            << " 它的BodyDir是 " << container[5] << "，HeadDir是 " << container[6] << ";";
         }
-        if (*itt.find("goal") != -1)
+        else if ((*itt).find("goal") != (*itt).npos)
         {
-            cout << container[1]
+        	goal.push_back((*itt));
+        	
+            cout << container[0] << " 距离我的 Direction 是" << container[1] << " ，Distance 是"
+				<< container[2] << ";";
         }
-        if (*itt.find("f ") != -1)
+        else if ((*itt).find("f ") != (*itt).npos)
         {
-
+        	flag.push_back((*itt));
+        	
+            cout << container[0] << " 距离我的Direction是 " << container[1] << " , Distance是"
+            		<< container[2] << ";";
         }
-        if (*itt.find("line") != -1)
+        else if ((*itt).find("Line") != (*itt).npos)
         {
-
+        	Line.push_back((*itt));
+        	
+			cout << container[0] << " 距离我的角度是 " << container[1] << ";"; 
         }
+		 
         ++itt;
     }
     
@@ -80,14 +122,16 @@ message_manager::message_manager(string original_message)
 {
 	original_message.replace(original_message.size()-1, 1, " ");
     messages.append(original_message.replace(0, 1, " "));
-    messages.replace(messages.find("see")-2, 2, " ");
+    messages.replace(messages.find("see")-2, 2, " ");                      //去左右两个括号
+    
+    //将字符串切割存到对应变量
     messages_hear.append(messages,messages.find("hear"), messages.find("see") - messages.find("hear"));
     messages_see.append(messages, messages.find("see"), messages.size() - messages.find("see"));
     
     int index = 0;
     int flag = 0;
     
-	while (index < messages_hear.size())                                 
+	while (index < messages_hear.size())            //将听到的信息分割                      
     {
         string temp = "";
         while (((messages_hear[index] != ' ') && (index < messages_hear.size())) || (flag != 0))
@@ -114,7 +158,7 @@ message_manager::message_manager(string original_message)
 	
 	index = 0;
 	flag = 0;
-	while (index < messages_see.size())                                 
+	while (index < messages_see.size())                        //将看到的信息分割  
     {
         string temp = "";
         while (((messages_see[index] != ' ') && (index < messages_see.size())) || (flag != 0))
@@ -139,34 +183,23 @@ message_manager::message_manager(string original_message)
         }	
 	}
 	
-	vector<string>::iterator itt = HEAR.begin();             //迭代器访问vector
+	vector<string>::iterator itt = HEAR.begin();             //将HEAR到对应信息存储在对应成员变量里面
 	while (itt != HEAR.end())
 	{
         switch (itt - HEAR.begin()-1)
         {
         case 0:
-            time.append(*itt);
+            time.append((*itt));
             break;
         case 1:
-            sender.append(*itt);
+            sender.append((*itt));
             break;
         case 2:
-            message.append(*itt);
+            message.append((*itt));
             break;
         }
 		++itt;
 	}
-
-    cout << message <<endl;
-    cout << time << endl;
-    cout << sender << endl;
-
-    itt = SEE.begin()+1;
-    while (itt != SEE.end())
-    {
-        cout << *itt << "#";
-        ++itt;
-    }
 
 }
 
@@ -176,8 +209,10 @@ message_manager::~message_manager()
 
 int main()
 {
-	string s = "(hear 1022 -30 passto(23,24))(see 1022 ((ball) -20 20 1 -2) ((player hfut1 2) 45 23 0.5 1 2240 ) ((goal r) 12 20) ((Line r) -30))";
+	string s = "(hear 1022 -30 passto(23,24))(see 1022 ((ball) -20 20 1 -2) ((player hfut1 2) 45 23 0.5 1 22 40 ) 
+                        ((goal r) 12 20) ((Line r) -30))";
     message_manager s1(s);
+    s1.message_print();
     
     return 0;
 }
