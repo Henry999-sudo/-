9d098e37cef8210c3ace17e1c653b07b22fad5b6
1595199614@qq.com <1595199614@qq.com>
2020-09-23 17:45:00 +0800

    改完程序，这次布置输出格式

--- Robocup.cpp
+++ Robocup.cpp
@@ -12,8 +12,9 @@ private:
     string time;
     string sender;
     string message;
-    vector<string> total;              //经过处理后的消息分段存入vector中
-	vector<string> objlnfos;
+    vector<string> HEAR;              //经过处理后的消息分段存入vector中
+	vector<string> SEE;
+    vector<string> objlnfos;
     
     /* data */
 public:
@@ -24,10 +25,40 @@ public:
 /*(hear 1022 -30 passto(23,24))(see 1022 ((ball) -20 20 1 -2) ((player hfut1 2) 45 23 0.5 1 22
 40 ) ((goal r) 12 20) ((Line r) -30))
 */
-message_manager::message_print()
+void message_manager::message_print()
 {
-    cout << "在" << time <<  "" << sender << << message <<endl;
-    cout << "在" << time << "";
+    cout << "在" << time <<  " 周期 hear 从 " << sender << "方向 听到了 " << message << endl;
+    
+    cout << "在" << time << " 周期 see ";
+    vector<string>::iterator itt = SEE.begin()-2;
+    while (itt != SEE.end())
+    {
+        vector<string> temp;
+        int index = 0;
+        while ()
+        while (temp[index] != '(')
+        if (*itt.find("ball") != -1)
+        {
+            cout << " Ball 距离我的距离是 " << *itt.substr(*itt.find("ball"))
+        }
+        if (*itt.find("player") != -1)
+        {
+
+        }
+        if (*itt.find("goal Side") != -1)
+        {
+
+        }
+        if (*itt.find("f ") != -1)
+        {
+
+        }
+        if (*itt.find("line") != -1)
+        {
+
+        }
+        ++itt;
+    }
     
 }
 
@@ -38,55 +69,91 @@ message_manager::message_manager(string original_message)
     messages.replace(messages.find("see")-2, 2, " ");
     messages_hear.append(messages,messages.find("hear"), messages.find("see") - messages.find("hear"));
     messages_see.append(messages, messages.find("see"), messages.size() - messages.find("see"));
-
-    int i = 0;
-	while (i < messages.size())                                 //拆分字符串存入vector
+    
+    int index = 0;
+    int flag = 0;
+    
+	while (index < messages_hear.size())                                 
     {
-        ++i;
         string temp = "";
-        while (messages[i] != ' '&&i < messages.size())
+        while (((messages_hear[index] != ' ') && (index < messages_hear.size())) || (flag != 0))
         {
-            temp.append(messages,i,1);
-            ++i;
+            if (messages_hear[index] == '(')
+            {
+                ++flag;
+            }
+            if (messages_hear[index] == ')')
+            {
+                --flag;
+            }
+
+            temp.append(messages_hear,index,1);
+            ++index;
         }
-		total.push_back(temp);
+
+        ++index;
+        if (temp != "")
+        {
+            HEAR.push_back(temp);
+        }	
 	}
 	
-    total.erase(total.begin());              //删除hear
-    total.erase(total.begin() + 3);          //删除see
-    total.erase(total.begin());              //删除多余的一个time
-	vector<string>::iterator itt = total.begin();             //迭代器访问vector
-	while (itt != total.end())
+	index = 0;
+	flag = 0;
+	while (index < messages_see.size())                                 
+    {
+        string temp = "";
+        while (((messages_see[index] != ' ') && (index < messages_see.size())) || (flag != 0))
+        {
+            if (messages_see[index] == '(')
+            {
+                ++flag;
+            }
+            if (messages_see[index] == ')')
+            {
+                --flag;
+            }
+
+            temp.append(messages_see,index,1);
+            ++index;
+        }
+
+        ++index;
+        if (temp != "")
+        {
+            SEE.push_back(temp);
+        }	
+	}
+	
+	vector<string>::iterator itt = HEAR.begin();             //迭代器访问vector
+	while (itt != HEAR.end())
 	{
-		//cout << *itt << "##";  
-        switch (itt - total.begin())
+        switch (itt - HEAR.begin()-1)
         {
         case 0:
-            sender.append(*itt);
-            break;
-        case 1:
-            message.append(*itt);
-            break;
-        case 2:
             time.append(*itt);
             break;
-        default:
-            objlnfos.push_back(*itt);
+        case 1:
+            sender.append(*itt);
+            break;
+        case 2:
+            message.append(*itt);
             break;
         }
 		++itt;
 	}
 
-    cout << "time :" << time << "#";
-    cout << "sender" << sender << "#";
-    cout << "message:" << message << "#";
-    itt = objlnfos.begin();
-    while (itt != objlnfos.end())
+    cout << message <<endl;
+    cout << time << endl;
+    cout << sender << endl;
+
+    itt = SEE.begin()+1;
+    while (itt != SEE.end())
     {
-        cout << "*" << *itt << "*";
+        cout << *itt << "#";
         ++itt;
     }
-    
+
 }
 
 message_manager::~message_manager()
